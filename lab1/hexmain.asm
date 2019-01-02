d6cce7ef1aa94bb65f0364a8fd794d13b05d1d5b
Redwan Mohammedberhan <red.01yassin@gmail.com>
2019-01-02 23:54:11 +0100

    update

--- lab1/hexmain.asm
+++ lab1/hexmain.asm
@@ -5,18 +5,20 @@
 	.text
 main:
 	li	$a0,0		# change this to test different values
-
+	
 	jal	hexasc		# call hexasc
 	nop			# delay slot filler (just in case)	
 
 	move	$a0,$v0		# copy return value to argument register
 
 	li	$v0,11		# syscall with v0 = 11 will print out
-	syscall			# one byte from a0 to the Run I/O window
+	run			# one byte from a0 to the Run I/O window
 	
 stop:	j	stop		# stop after one run
 	nop			# delay slot filler (just in case)
 
-  # You can write your own code for hexasc here
-  #
-
+hexasc: 
+	li	$t1,0x30
+	add	$t2,$t1,$a0
+    add	$v0,$t2,$0
+    jr	$ra
